a24929a50222d48cc2634486c31337811f934017
afterva <aftermath4826@gmail.com>
2026-06-05 13:13:58 +0800

    Update: /backend/routes/api.php Change for test connection

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test-connection', function () {
+    return response()->json([
+        'status' => 'Success',
+        'message' => 'Next.js and Laravel are officially talking!'
+    ]);
+});
